ecd3951503be188958014ce87fc085cfc5638a7b
glebkostin <glebkos777@gmail.com>
2024-03-05 12:49:48 +0300

    Файлы со страницей для вывода ошибки

---
+++ source/components/pages/error/error.js
@@ -0,0 +1,12 @@
+import {errors, errorsHTML} from "../../../modules/config.js";
+
+export const Error = (response) => {
+    const template = Handlebars.templates.error;
+    const root = document.getElementById('root');
+    root.innerHTML = template({
+        "status": response.status,
+        "message": response.statusText,
+        "description": errors[response.body.code],
+    });
+    console.log(response.code);
+}
