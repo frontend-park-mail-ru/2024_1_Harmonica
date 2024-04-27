6d0af5a136425c066ccdcf5540c1d6a9266a57bf
glebkostin <glebkos777@gmail.com>
2024-04-27 14:39:54 +0300

    HAR-106: Почти работает

--- source/features/csatRating/ui/csatRating.js
+++ source/features/csatRating/ui/csatRating.js
@@ -1,11 +1,39 @@
-import {View} from '../../../app/View.js';
-
-export class CsatRating extends View{
-    constructor(...args) {
-        super(...args);
-    }
-
-    render() {
-
-    }
+let stars = [];
+for (let i = 1; i <= 5; ++i){
+    const star = document.querySelector(`#csat-star-${i}`);
+    stars.push(star);
 }
+let counter = 0;
+for (let i = 1; i <= 5; ++i){
+    const star = document.querySelector(`#csat-star-${i}`);
+    star.addEventListener('click', () => {
+        for (let j = i + 1; j <= 5; ++j){
+            const unpickStars = document.querySelector(`#csat-star-${j}`);
+            unpickStars.classList.add('star-unpick')
+        }
+        for (let j = 1; j <= i; ++j){
+            const unpickStars = document.querySelector(`#csat-star-${j}`);
+            unpickStars.classList.remove('star-unpick');
+        }
+        counter = i;
+    });
+}
+
+const submitButton = document.querySelector('#csat-submit')
+submitButton.addEventListener('click', async () => {
+    let response;
+    try {
+        const addOptions = {
+            method: 'POST',
+            body: {
+                rating: counter,
+            },
+        };
+        response = await fetch('https://harmoniums.ru/api/v1/CSAT', {
+            ...addOptions,
+        });
+    } catch (error) {
+        console.log('Ojibka');
+    }
+
+})
