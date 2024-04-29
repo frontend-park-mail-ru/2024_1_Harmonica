37c546e782c66ff28999554c87fdb570e96ccf33
glebkostin <glebkos777@gmail.com>
2024-04-29 23:40:17 +0300

    HAR-126: Сделал верхную часть чата

--- source/widgets/chatWindow/ui/chatWindow.js
+++ source/widgets/chatWindow/ui/chatWindow.js
@@ -1,6 +1,7 @@
 import {View} from '../../../app/View.js';
 import chatWindowTemplate from './chatWindow.handlebars';
 import './chatWindow.scss';
+import {Avatar} from '../../../entity/avatar/ui/avatar.js';
 
 export class ChatWindow extends View{
     constructor(rootID, ...args) {
@@ -9,6 +10,13 @@ export class ChatWindow extends View{
     }
 
     render() {
-        this.root.innerHTML = chatWindowTemplate({});
+        const user = {
+            user_id: 1,
+            user_nickname: 'New user',
+        }
+        this.root.innerHTML = chatWindowTemplate({user});
+
+        const avatar = new Avatar('chat__selected-avatar');
+        avatar.render();
     }
 }
