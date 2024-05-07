dd16d02e0667e43f86d9c3344919844594ab352d
glebkostin <glebkos777@gmail.com>
2024-05-07 13:20:01 +0300

    HAR-126: Поменял стили в регистрации и добавил рабочий глаз

--- source/entity/inputField/ui/inputField.js
+++ source/entity/inputField/ui/inputField.js
@@ -13,11 +13,10 @@ export class InputField extends View {
     }
 
     render(blockID) {
-        this.root.innerHTML = inputFieldTemplate(
-            {
+        this.root.innerHTML = inputFieldTemplate({
                 field: this.field,
                 isPassword: this.field.type === 'password',
-            });
+        });
         const input = this.root.querySelector('#' + this.field.inputField);
         if (this.field.hint) {
             const hint = this.root.querySelector('#' + this.field.hint);
@@ -40,8 +39,19 @@ export class InputField extends View {
         });
         if (this.field.type === 'password') {
             const eyeButton = this.root.querySelector('#eye');
+
+            const eyeOpen = document.querySelector('#eye-open');
+            const eyeClosed = document.querySelector('#eye-closed');
             eyeButton.addEventListener('click', () => {
-                eyeButton.classList.toggle('slash');
+                if (input.type === 'password'){
+                    eyeOpen.classList.add('eye__visibility-hidden');
+                    eyeClosed.classList.remove('eye__visibility-hidden');
+                    input.type = 'text';
+                } else {
+                    eyeOpen.classList.remove('eye__visibility-hidden');
+                    eyeClosed.classList.add('eye__visibility-hidden');
+                    input.type = 'password';
+                }
             });
         }
     }
