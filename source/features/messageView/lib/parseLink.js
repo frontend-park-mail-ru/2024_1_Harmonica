bab222517f50a692abd71846ab09324fb77f8b7b
Gavriil <gavriil.belov@yandex.ru>
2024-05-12 21:12:57 +0300

    HAR-138: Add parse message function.

---
+++ source/features/messageView/lib/parseLink.js
@@ -0,0 +1,19 @@
+import {frontendURL} from '../../../shared/config.js';
+
+export const parseMessageOnLink = (message) => {
+    if (typeof message !== 'string') {
+        return;
+    }
+    var result = {
+        value: '',
+        pin_id: undefined,
+    };
+    const regex = new RegExp('(?:^|\\s)' + frontendURL + '\/pin\/\\d+(?:$|\\s)', 'g');
+    if (message.search(regex) === -1) {
+        result.value = message.trim();
+        return result;
+    }
+    result.pin_id = parseInt(message.match(regex)[0].replace(/[^0-9]/g, ''), 10);
+    result.value = message.replace(regex, '').trim();
+    return result;
+}
